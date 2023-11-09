52c93db2362f23fa3b2afc89047560fc660f0af7
Павел Белоусов <pasha.arua@yandex.ru>
2023-11-09 15:23:58 +0300

    Перед сдачей были добавлены счетчики времени, подчищены неиспользуемые using-и. Проект сдан 15.09.23, фиксация сделана для закрепления в github 09.11.23

--- Try1RASP/CustomControls/MessageAndroid.cs
+++ Try1RASP/CustomControls/MessageAndroid.cs
@@ -1,10 +1,4 @@
 ﻿using Android.Widget;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Maui.Controls;
 using Try1RASP.CustomControls;
 
 [assembly: Microsoft.Maui.Controls.Dependency(typeof(MessageAndroid))]
